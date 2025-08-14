e2e1ea5f35031a671c37af945d0c65db3defcd39
SpectralPixel <encrypteddvjjrxv@gmail.com>
2025-08-14 12:32:50 +0200

    Add command_help() function for 'help' command

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+def command_help():
+    print(r"help - Prints the help dialogue")
+    print(r"exit - Exits the program")
+    print(r"ping {ip_addr:port} - Sends a message to an IP address")
+
 def take_command():
     cmd = input("\n> ")
     eval(f"command_{cmd}")
