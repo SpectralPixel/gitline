8f00bf9475270f7c2e491f73813fbcb2f50157d3
SpectralPixel <encrypteddvjjrxv@gmail.com>
2025-08-14 12:35:12 +0200

    Add command_exit() for 'exit' command

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
+
 def command_help():
     print(r"help - Prints the help dialogue")
     print(r"exit - Exits the program")
     print(r"ping {ip_addr:port} - Sends a message to an IP address")
+
+def command_exit():
+    sys.exit(0)
 
 def take_command():
     cmd = input("\n> ")
